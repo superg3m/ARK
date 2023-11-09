60ef38bfc38c200443fca8e9714165a980d6f34d
Jovanni D <superg3m@gmail.com>
2023-11-08 21:06:32 -0500

    Update Win32_XInput.h

--- Header/Win32_XInput.h
+++ Header/Win32_XInput.h
@@ -15,14 +15,24 @@
 typedef DWORD Type_XInputGetState(DWORD, XINPUT_STATE*);
 typedef DWORD Type_XInputSetState(DWORD, XINPUT_VIBRATION*);
 
+// TODO(Jovanni): Remove these function pointers I think
 struct Win32_XInput {
     Type_XInputGetState* XInput_Get_State;
     Type_XInputSetState* XInput_Set_State;
 };
 
+// NOTE(Jovanni): Template -> inscription_object_operation_data
+
+// TODO(Jovanni): Change function name to xinput_get_state_stub(DWORD, XINPUT_STATE*)
 internal DWORD XInputGetStateStub(DWORD, XINPUT_STATE*);
+
+// TODO(Jovanni): Change function name to xinput_set_state_stub(DWORD, XINPUT_VIBRATION*)
 internal DWORD XInputSetStateStub(DWORD, XINPUT_VIBRATION*);
+
+// TODO(Jovanni): Change function name to inscription_xinput_load()
 internal void LoadXInput(Win32_XInput* xInput);
+
+// TODO(Jovanni): Change function name to inscription_xinput_create()
 Win32_XInput create_XInput();
 
 // Functions here
